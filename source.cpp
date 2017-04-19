87def13aaed59702ba9159b53d76d3e974856e58
defoye <erniedefoy@yahoo.com>
2017-04-19 01:43:39 -0400

    Update source.cpp

--- source.cpp
+++ source.cpp
@@ -18,6 +18,59 @@ struct plane {
 } *Arrival, *departure;
 
 
+int TIME;
+int takeOffWaitTime;
+int landingWaitTime;
+int numCrash;
+int numDeparting;
+int numArriving;
+int numPeopleSafe;
+int numPeopleKilled;
+int numGrandKilled;
+int waitTimeGrand;
+int numCargoSafe;
+int cargoDestroyed;
+
+
+
+/*
+ *  This can be a function that increments the time.  Here, we would
+ *  update fuel, time, vectors, etc.  We can call other methods like
+ *  a sort function or a updateStats function.  Each turn, it will
+ *  delete two plane structs.  
+ *
+ *  In order to determine how many planes can depart, we need to consider
+ *  out how many planes are arriving.  If 7 planes with the same amount
+ *  of fuel are arriving in 5 time units, we will need to land at least
+ *  two planes immediately.
+ *
+ *
+ */
+void next()
+{
+    
+}
+
+/*
+ *  Sorts the planes into a queue(the first plane will be the next to
+ *  arrive or depart).  Prioritizes grandchildren, passengers, cargo, 
+ *  and wait time.
+ */
+void sortPlanes()
+{
+    
+}
+
+
+
+/*
+ *
+ */
+void updateStats()
+{
+    
+}
+
 
 int main() {
     ifstream input;
@@ -27,6 +80,7 @@ int main() {
     vector<plane> departing;
     
     input.open("/Users/erniedefoy/Desktop/planes.txt", ios::in);//you need a path to your file here
+    
     //while (!input.eof())
     while(input.good())
     {
@@ -64,15 +118,17 @@ int main() {
         
     }
     
-   
     
+    /*
     for(plane pl: arriving) {
         cout << "hi"; //test prints "hi" for each arriving plane
     }
-    
+    */
+     
     input.close();
     
     cout << endl;
     
 }
+
 
